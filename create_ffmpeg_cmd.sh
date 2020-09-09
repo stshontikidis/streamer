3f28277ac163583de4fa0d9ff9985fcc3d73a40f
Sava Tshontikidis <sava.anthony@gmail.com>
2020-09-08 22:54:24 -0400

    Remove multiple mime types

--- create_ffmpeg_cmd.sh
+++ create_ffmpeg_cmd.sh
@@ -34,6 +34,6 @@ fi
 
 #FFMPEG_CMD="ffmpeg -rtsp_transport tcp -i ${INPUT} ${LACKING_AUDIO} -acodec ${DEFAULT_AUDIO} -vcodec ${DEFAULT_VIDEO} -hls_list_size 2 -hls_init_time 1 -hls_time 1 -hls_flags delete_segments ${OUTPUT_PATH}${OUTPUT}.m3u8"
 
-FFMPEG_CMD="ffmpeg -rtsp_transport tcp -i rtsp://192.168.1.5:7447/5e8e5257e4b088237a8892cc_1 -codec:a ${DEFAULT_AUDIO} -vn -hls_list_size 2 -hls_init_time 1 -hls_time 1 -hls_flags delete_segments  ${OUTPUT_PATH}${OUTPUT}.m3u8"
+FFMPEG_CMD="ffmpeg -rtsp_transport tcp -i rtsp://192.168.1.5:7447/5e8e5257e4b088237a8892cc_1 -codec:a ${DEFAULT_AUDIO} -vn -hls_list_size 2 -hls_init_time 1 -hls_time 2 -hls_flags delete_segments  ${OUTPUT_PATH}${OUTPUT}.m3u8"
 
 echo "${FFMPEG_CMD}"
